6c0f527272342d3fe1dfc8764d3369ab93fd68b4
MN <D1248534@o365.fcu.edu.tw>
2025-05-07 01:32:11 +0800

    new progect 新增css 新增圖標 新增提醒

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("loginForm");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); // 阻止表單送出
+        alert("你被騙了！");
+    });
+});
